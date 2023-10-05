f6bac4c9a84fc3440be3e1df7cec593896e2337d
AntipasG <antipasbelihu@gmail.com>
2023-10-05 23:47:30 +0300

    Advanc 1 assemblicode

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ *main -void
+ *Return: 1
+ **/
+int main(void) 
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 53);
+	return (1);
+}
