27c4488c094c3e00fe95db9e69f2371a9972694e
AntipasG <antipasbelihu@gmail.com>
2023-10-07 00:27:09 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ *main - void
+ *
+ *
+ *
+ *
+ *Return: 0
+ **/
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
