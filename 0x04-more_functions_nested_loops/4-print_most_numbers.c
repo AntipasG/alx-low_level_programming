08bc347ec734ef92fc576c53431efadeb10fdb5b
AntipasG <antipasbelihu@gmail.com>
2023-10-17 22:16:29 +0300

    printing numbers with out 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
+ *print_most_numbers - printing number with out 2 and 4
  *
  *
- *
- *
+ *Return: void
  **/
 void print_most_numbers(void)
 {
 	int a = 0;
 
-	while(a <= 9)
+	while (a <= 9)
 	{
 		if (a != 4 && a != 2)
 		{
