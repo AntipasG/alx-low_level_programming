100cc50e532d4684cc74cef2e46b8c9d72d815af
AntipasG <antipasbelihu@gmail.com>
2023-10-17 23:38:53 +0300

    0 rest to 98 and Readme

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - this is changing the value of *n
+ *@n: this is the variable that will be change
+ *
+ *Return: void
+ **/
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+This is all about 
+	*POINTERS
+	*ARRAYS
+	*STRING
