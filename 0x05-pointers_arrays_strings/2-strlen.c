f983aa4268e28c1159744f13999497db205b3d59
AntipasG <antipasbelihu@gmail.com>
2023-10-17 23:45:38 +0300

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - this is finding the string length
+ *@s: this is the string address
+ *
+ *
+ *Return: len the length
+ **/
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+}
