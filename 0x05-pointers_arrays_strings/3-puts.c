fa80bc3a4af6ddd8a2233bf9a8a083bc46d7d799
AntipasG <antipasbelihu@gmail.com>
2023-10-17 23:46:26 +0300

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - this is printing a string
+ *@str: this is the varable string that is printed
+ *
+ *
+ *Return: void
+ **/
+
+void _puts(char *str)
+{
+	puts(str);
+}
