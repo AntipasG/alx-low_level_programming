8f232a7c60d00f099be0a1c750fd276a783aaa5d
AntipasG <antipasbelihu@gmail.com>
2023-10-18 18:09:27 +0300

    printing line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  *print_line - printing underscore lines
- *
+ *@n: the parameter
  *Return: void
  **/
 
@@ -10,7 +10,7 @@ void print_line(int n)
 {
 	int a;
 
-	for(a = 0; a < n; a++)
+	for (a = 0; a < n; a++)
 	{
 		_putchar('_');
 	}
