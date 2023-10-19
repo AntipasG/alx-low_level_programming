4ac66fad658b7dfe60dce1389fa91fd913ceaa60
AntipasG <antipasbelihu@gmail.com>
2023-10-20 01:51:53 +0300

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ *_strcmp - Writeing a function that compares two strings.
+ *@s1: the first parameter of the string
+ *@s2: the second parameter of string
+ *
+ *Return: comp.
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+	int comp = strcmp(s1, s2);
+
+	return (comp);
+}
